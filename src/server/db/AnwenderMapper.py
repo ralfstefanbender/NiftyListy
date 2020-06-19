9998ace5ba45f714bc9a821a75cdc54ac95f19fd
Maria Weinberger <65721399+mariaweinberger@users.noreply.github.com>
2020-06-19 20:26:55 +0200

    Create AnwenderMapper.py

---
+++ src/server/db/AnwenderMapper.py
@@ -0,0 +1,14 @@
+from server.bo.Transaction import Transaction
+from server.db.Mapper import Mapper
+
+
+class AnwenderMapper (Mapper):
+    """Mapper-Klasse, die Anwender-Objekte auf eine relationale
+    Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfügung
+    gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
+    gelöscht werden können. Das Mapping ist bidirektional. D.h., Objekte können
+    in DB-Strukturen und DB-Strukturen in Objekte umgewandelt werden.
+    """
+
+    def __init__(self):
+        super().__init__()
