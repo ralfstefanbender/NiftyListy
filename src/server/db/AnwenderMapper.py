9cd2ca9ce1a251c7b3a916621dd9fc5b663fe1bc
Maria Weinberger <65721399+mariaweinberger@users.noreply.github.com>
2020-06-26 19:21:34 +0200

    Update AnwenderMapper.py
    Ergänzung der Funktion, dass Anwender durch die ID gefunden werden. Kommentare zur Klärung ergänzt.

--- src/server/db/AnwenderMapper.py
+++ src/server/db/AnwenderMapper.py
@@ -13,7 +13,7 @@ class AnwenderMapper (Mapper):
     def __init__(self):
         super().__init__()
 
-    def auslesen(self):
+    def find_all_anwender(self):
 
         result = []
         cursor = self._cnx.cursor()
@@ -25,6 +25,29 @@ class AnwenderMapper (Mapper):
             account.set_id(id)
             account.set_owner(owner)
             result.append(account) """
+        
+        """ Klare Definition der Rollenbetitelung fehlt  """
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+    
+    def find_anwender_by_anwender_id(self, Anwender_ID):
+
+        result = []
+        cursor = self._cnx.cursor()
+        command = "PLatzhalter für SQL Code".format(Anwender_ID)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        """ for (id, anwender) in tuples:
+            account = Account()
+            account.set_id(id)
+            account.set_anwender(anwender)
+            result.append(account) """
+        
+        """ Klare Definition der Rollenbetitelung fehlt  """
 
         self._cnx.commit()
         cursor.close()
