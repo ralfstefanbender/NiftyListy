64763746d17fb8f3e8a88e9e1656610fb5ca5ccc
RahelUen <60141481+RahelUen@users.noreply.github.com>
2020-07-01 22:58:22 +0200

    Frontend: Einkaufsgruppe,-liste,-händle BO

--- frontend/src/api/EinkaufsgruppeBO.js
+++ frontend/src/api/EinkaufsgruppeBO.js
@@ -1,1 +1,50 @@
 import BusinessObject from './BusinessObject';
+
+export default class EinkaufsgruppeBO extends BusinessObject {
+
+    constructor(einkaufsgruppe_name, teilnehmerliste, adminliste) {
+        super();
+        this.einkaufsgruppe_name = einkaufsgruppe_name;
+        this.teilnehmerliste = teilnehmerliste;
+        this.adminliste = adminliste;
+    }
+
+    /**addUser(Anwender) {
+
+    }
+
+    delUser(Anwender) {
+
+    }
+
+    promoteToAdmin(Anwender) {
+
+    }**/
+
+    getEinkaufsgruppeName() {
+        return this.einkaufsgruppe_name
+    }
+
+    setEinkaufsgruppeName(name) {
+        this.einkaufsgruppe_name = name
+    }
+
+    // Returns an Array of EinkaufsgruppeBO from a given JSON structure
+    static fromJSON(einkaufsgruppe) {
+        let result = [];
+
+        if (Array.isArray(einkaufsgruppe)) {
+            einkaufsgruppe.forEach((eg) => {
+                Object.setPrototypeOf(eg, EinkaufsgruppeBO.prototype)
+                result.push(eg)
+            })
+        } else {
+            // Es handelt sich offenbar um ein singuläres Objekt
+            let eg = einkaufsgruppe
+            Object.setPrototypeOf(eg, EinkaufsgruppeBO.prototype)
+            result.push(eg)
+        }
+
+        return result;
+    }
+}
--- frontend/src/api/EinzelhändlerBO.js
+++ frontend/src/api/EinzelhändlerBO.js
@@ -1,1 +1,42 @@
 import BusinessObject from './BusinessObject';
+
+/**Ist dieses BO notwenig? 
+ * 
+ * Alle Methoden genau dieselben wie bei der Einkaufsliste.
+ * Enthalten die BO andere Methoden neben get und set?
+ */
+
+export default class EinkaufsgruppeBO extends BusinessObject {
+
+    //this.itemID = 0
+    //this.id = 0
+    
+    constructor(name) {
+        this.name = name
+        //this.items ist ein dictionary
+        /**Wie geht hier die Umsetzung:
+        self.__id = Einkaufsliste.id
+        Einkaufsliste.id += 1 */
+    }
+
+
+
+    // Returns an Array of EinzelhändlerBO from a given JSON structure
+    static fromJSON(einzelhaendler) {
+        let result = [];
+
+        if (Array.isArray(einzelhaendler)) {
+            einzelhaendler.forEach((eh) => {
+                Object.setPrototypeOf(eh, EinzelhändlerBO.prototype)
+                result.push(eh)
+            })
+        } else {
+            // Es handelt sich offenbar um ein singuläres Objekt
+            let eh = einzelhaendler
+            Object.setPrototypeOf(eh, EinzelhändlerBO.prototype)
+            result.push(eh)
+        }
+
+        return result;
+    }
+}
