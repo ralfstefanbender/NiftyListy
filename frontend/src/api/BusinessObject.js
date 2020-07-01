69a630bb72747b0250be167d192223ac9c97e3de
Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>
2020-07-01 18:20:28 +0200

    Frontend BusinessObject

---
+++ frontend/src/api/BusinessObject.js
@@ -0,0 +1,31 @@
+export default class BusinessObject {
+
+    constructor() {
+        this.id = 0;
+        this.erstellungszeitpunkt
+    }
+
+    setID(id) {
+        this.id = id
+    }
+
+    getID(id) {
+        return this.id
+    }
+
+    
+    getErstellungszeitpunkt(erstellungszeitpunkt) {
+        return this.erstellungszeitpunkt
+    }
+    
+    
+    toString() {
+        let result = ''
+        for (var prop in this) {
+            result += prop + ': ' + this[prop] + ' ';
+        }
+        return result;
+    }
+
+
+}
---
+++ frontend/src/api/EinzelhändlerBO.js
@@ -0,0 +1,1 @@
+import BusinessObject from './BusinessObject';
