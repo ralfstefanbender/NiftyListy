d1a5d1dfcc890010b50845daf949394d3ab7a97a
Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>
2020-07-10 18:29:06 +0200

    Material Ui und Firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,165 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Container, ThemeProvider, CssBaseline } from '@material-ui/core';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import About from './components/pages/About';
+import SignIn from './components/pages/SignIn';
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Nifty Listy</h1>
-        <p>Einkaufslisten App oder sowas</p>
-      </header>
-    </div>
-  );
+/**
+ * The main bank administration app. It uses Googles firebase to log into the bank end. For routing the 
+ * user to the respective pages, react-router-dom ist used.
+ * 
+ * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
+ * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
+ * @see [react-router-dom](https://reacttraining.com/react-router/web/guides/quick-start)
+ */
+class App extends React.Component {
+
+  // Your web app's Firebase configuration
+	#firebaseConfig = {
+		apiKey: "AIzaSyAAAr_RJEaWIYvlLxUVWZ23QVogax55aSs",
+		authDomain: "niftylisty.firebaseapp.com",
+		databaseURL: "https://niftylisty.firebaseio.com",
+		projectId: "niftylisty",
+		storageBucket: "niftylisty.appspot.com",
+		messagingSenderId: "806709174428",
+		appId: "1:806709174428:web:a8bcdd21a20770410b8d59",
+		measurementId: "G-BMW5Q1S622"
+	};
+
+
+	/** Constructor of the app, which initializes firebase  */
+	constructor(props) {
+		super(props);
+
+		// Init an empty state
+		this.state = {
+			currentUser: null,
+			appError: null,
+			authError: null,
+			authLoading: false
+		};
+	}
+
+	/** 
+	 * Create an error boundary for this app and recieve all errors from below the component tree.
+	 * 
+	 * @See See Reacts [Error Boundaries](https://reactjs.org/docs/error-boundaries.html)
+ 	 */
+	static getDerivedStateFromError(error) {
+		// Update state so the next render will show the fallback UI.
+		return { appError: error };
+	}
+
+	/** Handles firebase usres logged in state changes  */
+	handleAuthStateChange = user => {
+		if (user) {
+			this.setState({
+				authLoading: true
+			});
+			// The user is signed in
+			user.getIdToken().then(token => {
+				// Add the token to the browser's cookies. The server will then be
+				// able to verify the token against the API.
+				// SECURITY NOTE: As cookies can easily be modified, only put the
+				// token (which is verified server-side) in a cookie; do not add other
+				// user information.
+				document.cookie = `token=${token};path=/`;
+
+				// Set the user not before the token arrived 
+				this.setState({
+					currentUser: user,
+					authError: null,
+					authLoading: false
+				});
+			}).catch(e => {
+				this.setState({
+					authError: e,
+					authLoading: false
+				});
+			});
+		} else {
+			// User has logged out, so clear the id token
+			document.cookie = 'token=;path=/';
+
+			// Set the logged out user to null
+			this.setState({
+				currentUser: null,
+				authLoading: false
+			});
+		}
+	}
+
+  /** 
+   * Handles the sign in request of the SignIn component uses the firebase.auth() component to sign in.
+	 * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
+	 * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
+	 */
+	handleSignIn = () => {
+		this.setState({
+			authLoading: true
+		});
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+	}
+
+	/**
+	 * Lifecycle method, which is called when the component gets inserted into the browsers DOM.
+	 * Initializes the firebase SDK.
+	 * 
+	 * @see See Googles [firebase init process](https://firebase.google.com/docs/web/setup)
+	 */
+	componentDidMount() {
+		firebase.initializeApp(this.#firebaseConfig);
+		firebase.auth().languageCode = 'en';
+		firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
+	}
+
+	/** Renders the whole app */
+	render() {
+		const { currentUser, appError, authError, authLoading } = this.state;
+
+		return (
+			<ThemeProvider theme={Theme}>
+				{/* Global CSS reset and browser normalization. CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Router basename={process.env.PUBLIC_URL}>
+					<Container maxWidth='md'>
+						<Header user={currentUser} />
+						{
+							// Is a user signed in?
+							currentUser ?
+								<>
+									<Redirect from='/' to='customers' />
+									<Route exact path='/customers'>
+										<CustomerList />
+									</Route>
+									<Route path='/transactions'>
+										<TransactionList />
+									</Route>
+									<Route path='/accounts'>
+										<AllAccountList />
+									</Route>
+									<Route path='/about' component={About} />
+								</>
+								:
+								// else show the sign in page
+								<>
+									<Redirect to='/index.html' />
+									<SignIn onSignIn={this.handleSignIn} />
+								</>
+						}
+						<LoadingProgress show={authLoading} />
+						<ContextErrorMessage error={authError} contextErrorMsg={`Something went wrong during sighn in process.`} onReload={this.handleSignIn} />
+						<ContextErrorMessage error={appError} contextErrorMsg={`Something went wrong inside the app. Please reload the page.`} />
+					</Container>
+				</Router>
+			</ThemeProvider>
+		);
+	}
 }
 
 export default App;
+
