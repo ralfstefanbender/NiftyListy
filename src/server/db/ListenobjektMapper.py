990be9d42b6456cfe9a70adfb7a7f7ef387df799
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2020-07-13 21:51:25 +0200

    Mapper für Listenobjekt erstellt

---
+++ src/server/db/ListenobjektMapper.py
@@ -0,0 +1,33 @@
+from server.bo.Listenobjekt import Einzelhändler
+from server.db.Mapper import Mapper
+
+
+class ListenobjektMapper (Mapper):
+
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+
+        pass    
+    
+    def find_by_key(self, Einzelhändler_ID):
+        
+        pass
+
+    def find_by_name(self, name):
+        
+        pass
+
+    def insert(self, anwender):
+        
+        pass
+    
+    def update(self, object):
+        
+        pass
+
+    def delete(self, object):
+
+        pass
