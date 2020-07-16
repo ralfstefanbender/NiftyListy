09cb3a4b0ce63231369fa95479911000eecf6ae5
Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>
2020-07-16 22:37:28 +0200

    Group Components

--- frontend/src/components/GroupList.js
+++ frontend/src/components/GroupList.js
@@ -5,10 +5,10 @@ import AddIcon from '@material-ui/icons/Add';
 import ClearIcon from '@material-ui/icons/Clear'
 import { withRouter } from 'react-router-dom';
 import { ListAPI } from '../api';
-/*import ContextErrorMessage from './dialogs/ContextErrorMessage';
-import LoadingProgress from './dialogs/LoadingProgress';
-import ArtikelForm from './dialogs/ArtikelForm';
-import ListenEintrag from './ListenEintrag';*/
+import InlineError from './dialogs/InlineError';
+import LoadingBar from './dialogs/LoadingBar';
+import GruppeForm from './dialogs/GruppeForm';
+import GroupListEntry from './GroupListEntry';
 
 
 class GroupList extends Component {
@@ -19,34 +19,34 @@ class GroupList extends Component {
 
     let expandedID = null;
 
-    if (this.props.location.expandArtikel) {
-      expandedID = this.props.location.expandArtikel.getID();
+    if (this.props.location.expandGruppe) {
+      expandedID = this.props.location.expandGruppe.getID();
     }
 
     // Initialisierung eines leeren Zustandes (state)
     this.state = {
-      ArtikelArray: [],
-      filteredArtikel: [],
-      ArtikelFilterStr: '',
+      GruppenArray: [],
+      filteredGruppe: [],
+      GruppeFilterStr: '',
       error: null,
       loadingInProgress: false,
-      expandedArtikelID: expandedID,
-      showArtikelForm: false
+      expandedGruppeID: expandedID,
+      showGruppeForm: false
     };
   }
 
-  /** Fetch Befehl für alle ArtikelBOs aus dem Backend */
-  getArtikel = () => {
-    ListAPI.getAPI().getArtikel()
-      .then(articleBO =>
-        this.setState({               // Erstellt einen neuen State sobald alle ArtikelBOs gefetched sind
-          ArtikelArray: articleBO,
-          filteredArtikel: [...articleBO], // speichert eine Kopie
+  /** Fetch Befehl für alle EinkaufsgruppeBOs aus dem Backend */
+  getGruppe = () => {
+    ListAPI.getAPI().getGruppe()
+      .then(EinkaufsgruppeBO =>
+        this.setState({               // Erstellt einen neuen State sobald alle GruppeBOs gefetched sind
+          GruppenArray: EinkaufsgruppeBO,
+          filteredGruppe: [...EinkaufsgruppeBO], // speichert eine Kopie
           loadingInProgress: false,   // Der Ladebalken wird "vrborgen"
           error: null
         })).catch(e =>
           this.setState({             // Setzt den Zustand mit dem entsprechenden Error zurück 
-            ArtikelArray: [],
+            GruppenArray: [],
             loadingInProgress: false, // Der Ladebalken wird wieder "verborgen"
             error: e
           })
@@ -61,76 +61,76 @@ class GroupList extends Component {
 
   /** Lifecycle Methode, die aufgerufen wird, wenn die Komponente dem Browser DOM hizugefügt wird */
   componentDidMount() {
-    this.getArtikel();
+    this.getGruppe();
   }
 
   /** 
    * Verwaltet das onExpandedStateChange Event Einer Listeneintrags Komponente. 
-   * Steuert den geöffneten Zustand eines Listeneintrags des betroffenen ArtikelBOs
+   * Steuert den geöffneten Zustand eines Listeneintrags des betroffenen GruppeBOs
    */
-  onExpandedStateChange = Artikel => {
+  onExpandedStateChange = Gruppe => {
     // Der Defaultwert für den geöffneten Zustand wird auf Null gesetzt
     let newID = null;
 
     // Wen der gleiche Eintrag angeklickt wird, so wird er geschlossen, ansonsten wird ein neuer geöffnet
-    if (Artikel.getID() !== this.state.expandedArtikelID) {
-      // Öffnet den Listeneintrag des Artikels über die ArtikelID
-      newID = Artikel.getID();
+    if (Gruppe.getID() !== this.state.expandedGruppeID) {
+      // Öffnet den Listeneintrag des Gruppes über die GruppeID
+      newID = Gruppe.getID();
     }
 
     this.setState({
-      expandedArtikelID: newID,
+      expandedGruppeID: newID,
     });
   }
 
   /** 
-   * Verwaltet die "ArtikelDeleted" Ereignisse der Listeneintrags Komponente
+   * Verwaltet die "GruppeDeleted" Ereignisse der Listeneintrags Komponente
    */
-  ArtikelDeleted = Artikel => {
-    const newArtikelArray = this.state.ArtikelArray.filter(ArtikelFormState => ArtikelFormState.getID() !== Artikel.getID());
+  GruppeDeleted = Gruppe => {
+    const newGruppenArray = this.state.GruppenArray.filter(GruppeFormState => GruppeFormState.getID() !== Gruppe.getID());
     this.setState({
-      ArtikelArray: newArtikelArray,
-      filteredArtikel: [...newArtikelArray],
-      showArtikelForm: false
+      GruppenArray: newGruppenArray,
+      filteredGruppe: [...newGruppenArray],
+      showGruppeForm: false
     });
   }
 
-  /** Verwalted das Artikelhinzufügen Ereignis wenn der Knopf betätigt wird */
-  addArtikelButtonClicked = event => {
+  /** Verwalted das Gruppehinzufügen Ereignis wenn der Knopf betätigt wird */
+  addGruppeButtonClicked = event => {
     // Schließe nicht den geöffneten Zustand
     event.stopPropagation();
-    //Zeige den neuen Artikeleintrag
+    //Zeige den neuen Gruppeeintrag
     this.setState({
-      showArtikelForm: true
+      showGruppeForm: true
     });
   }
 
-  /** Verwaltet das "Schließen" Ereignis beim erstellen neuer Artikel */
-  ArtikelFormClosed = Artikel => {
-    // Der jeweilige Artikel ist nicht null und wurde daher erstellt
-    if (Artikel) {
-      const newArtikelArray = [...this.state.ArtikelArray, Artikel];
+  /** Verwaltet das "Schließen" Ereignis beim Erstellen einer neuen Gruppe */
+  GruppeFormClosed = Gruppe => {
+    // Die jeweilige Gruppe ist nicht null und wurde daher erstellt
+    if (Gruppe) {
+      const newGruppenArray = [...this.state.GruppenArray, Gruppe];
       this.setState({
-        ArtikelArray: newArtikelArray,
-        filteredArtikel: [...newArtikelArray],
-        showArtikelForm: false
+        GruppenArray: newGruppenArray,
+        filteredGruppe: [...newGruppenArray],
+        showGruppeForm: false
       });
     } else {
       this.setState({
-        showArtikelForm: false
+        showGruppeForm: false
       });
     }
   }
 
-  /** Verwaltet das Filter Ereignis beim filtern von Artikeln */
+  /** Verwaltet das Filter Ereignis beim filtern von Gruppen */
   filterFieldValueChange = event => {
     const value = event.target.value.toLowerCase();
     this.setState({
-      filteredArtikel: this.state.ArtikelArray.filter(Artikel => {
-        let ArtikelNameContainsValue = Artikel.getArtikelName().toLowerCase().includes(value);
-        return ArtikelNameContainsValue;
+      filteredGruppe: this.state.GruppenArray.filter(Gruppe => {
+        let GruppeNameContainsValue = Gruppe.getGruppeName().toLowerCase().includes(value);
+        return GruppeNameContainsValue;
       }),
-      ArtikelFilter: value
+      GruppeFilter: value
     });
   }
 
@@ -138,31 +138,31 @@ class GroupList extends Component {
   clearFilterFieldButtonClicked = () => {
     // Der Filter wird zurückgesetzt
     this.setState({
-      filteredArtikel: [...this.state.ArtikelArray],
-      ArtikelFilter: ''
+      filteredGruppe: [...this.state.GruppenArray],
+      GruppeFilter: ''
     });
   }
 
   /** Rendern der gesamten Komponente GroupList */
   render() {
     const { classes } = this.props;
-    const { filteredArtikel, ArtikelFilter, expandedArtikelID, loadingInProgress, error, showArtikelForm } = this.state;
+    const { filteredGruppe, GruppeFilter, expandedGruppeID, loadingInProgress, error, showGruppeForm } = this.state;
 
     return (
       <div className={classes.root}>
-        <Grid className={classes.ArtikelFilter} container spacing={1} justify='flex-start' alignItems='center'>
+        <Grid className={classes.GruppeFilter} container spacing={1} justify='flex-start' alignItems='center'>
           <Grid item>
             <Typography>
-              Filter Artikel nach Namen:
+              Filtern der Gruppen nach Namen:
               </Typography>
           </Grid>
           <Grid item xs={4}>
             <TextField
               autoFocus
               fullWidth
-              id='ArtikelFilter'
+              id='GruppeFilter'
               type='text'
-              value={ArtikelFilter}
+              value={GruppeFilter}
               onChange={this.filterFieldValueChange}
               InputProps={{
                 endAdornment: <InputAdornment position='end'>
@@ -175,23 +175,23 @@ class GroupList extends Component {
           </Grid>
           <Grid item xs />
           <Grid item>
-            <Button variant='contained' color='primary' startIcon={<AddIcon />} onClick={this.addArtikelButtonClicked}>
-              Artikel hinzufügen
+            <Button variant='contained' color='primary' startIcon={<AddIcon />} onClick={this.addGruppeButtonClicked}>
+              Gruppe hinzufügen
           </Button>
           </Grid>
         </Grid>
         { 
           // Zeige die Liste der ListenEintrags Komponenten
-          // Verwende keinen expliziten Abgleich da die "expandedArtikelID" auch in Str Form vorliegen könnte, wenn sie aus URL Parametern entnommen wurde
-          filteredArtikel.map(Artikel =>
-            <ListenEintrag key={Artikel.getID()} Artikel={Artikel} expandedState={expandedArtikelID === Artikel.getID()}
+          // Verwende keinen expliziten Abgleich da die "expandedGruppeID" auch in Str Form vorliegen könnte, wenn sie aus URL Parametern entnommen wurde
+          filteredGruppe.map(Gruppe =>
+            <GroupListEntry key={Gruppe.getID()} Gruppe={Gruppe} expandedState={expandedGruppeID === Gruppe.getID()}
               onExpandedStateChange={this.onExpandedStateChange}
-              onArtikelDeleted={this.ArtikelDeleted}
+              onGruppeDeleted={this.GruppeDeleted}
             />)
         }
-        <LoadingProgress show={loadingInProgress} />
-        <ContextErrorMessage error={error} contextErrorMsg={`Die Artikel konnten nicht geladen werden`} onReload={this.getArtikel} />
-        <ArtikelForm show={showArtikelForm} onClose={this.ArtikelFormClosed} />
+        <LoadingBar show={loadingInProgress} />
+        <InlineError error={error} InlineError={`Die Gruppe konnte nicht geladen werden`} onReload={this.getGruppe} />
+        <GruppeForm show={showGruppeForm} onClose={this.GruppeFormClosed} />
       </div>
     );
   }
@@ -202,7 +202,7 @@ const styles = theme => ({
   root: {
     width: '100%',
   },
-  ArtikelFilter: {
+  GruppeFilter: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(1),
   }
